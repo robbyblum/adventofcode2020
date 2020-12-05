a983e897e6e8a48e377790c056507be2191ff639
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-05 15:57:44 -0500

    day 3 part 2 done

--- day3.py
+++ day3.py
@@ -1,28 +1,52 @@
 # day 3...
 
-with open("input/day3.txt") as f:
-    input = f.readlines()
 
-# remove \n from lines, idk, maybe this'll help me in part 2?
-for i, line in enumerate(input):
-    input[i] = line.strip()
+# OK for part 2 I need to have this in a function
+def check_trees(map, slopex, slopey):
 
-# starting position...this is overkill probably
-x0, y0 = 0, 0
+    # starting position, hardcoded for now
+    x0, y0 = 0, 0
+    # initialize at starting position
+    x, y, = x0, y0
 
-# dimensions of the array, I guess
-xmax = len(input[0])  # 31
-ymax = len(input)  # 323
+    # dimensions of the array
+    xmax = len(map[0])
+    ymax = len(map)
 
-# initialize at starting position
-x, y, = x0, y0
+    n_trees = 0
+    # step through list and check for trees. move right modulo xmax, to handle
+    # of the periodicity thing
+    for y in range(slopey, ymax, slopey):
+        x = (x + slopex) % xmax
+        if map[y][x] == "#":
+            n_trees += 1
 
-n_trees = 0
-# step through list and check for trees. move right modulo xmax, to handle
-# of the periodicity thing
-for y in range(1, ymax):
-    x = (x + 3) % xmax
-    if input[y][x] == "#":
-        n_trees += 1
+    return n_trees
 
-print(n_trees)
+
+def main():
+    from math import prod
+
+    with open("input/day3.txt") as f:
+        input = f.readlines()
+
+    # remove \n from lines, so that len(input[i]) gives the actual width
+    for i, line in enumerate(input):
+        input[i] = line.strip()
+
+    # part 1
+    n_trees = check_trees(input, 3, 1)
+    print("Part 1:", n_trees)
+
+    # part 2:
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    trees = []
+
+    for slope in slopes:
+        trees.append(check_trees(input, *slope))
+
+    print("Part 2:", prod(trees), trees)
+
+
+if __name__ == '__main__':
+    main()
