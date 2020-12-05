7233080730673fc661063288d3025a8ea2498ae5
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-05 18:27:31 -0500

    day 5, part 2 done

--- day5.py
+++ day5.py
@@ -14,6 +14,8 @@ def convert_col(colstr):
 
 
 def main():
+    import numpy as np
+
     with open("input/day5.txt") as f:
         input = f.readlines()
         input = [i.strip() for i in input]
@@ -31,7 +33,15 @@ def main():
         seats_id.append(seat_id(row, col))
 
     # Part 1:
-    print(max(seats_id))
+    print("Part 1:", max(seats_id))
+
+    # Part 2: this can be done in fewer lines but who cares
+    seats_id.sort()
+    dseats = np.diff(np.array(seats_id))
+    i_seat_before = np.nonzero(dseats - 1)[0][0]
+    seat_before = seats_id[i_seat_before]
+    my_seat = seat_before + 1
+    print("Part 2:", my_seat)
 
 
 if __name__ == '__main__':
