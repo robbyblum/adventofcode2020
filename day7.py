b97276d9ca4f9e67cc1b959fa389f4128b53ff0e
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-08 00:14:56 -0500

    day 7 part 1 done finally

--- day7.py
+++ day7.py
@@ -10,7 +10,7 @@ def build_rules(file):
     # the stuff it contains
     ruledict = {}
     for line in raw:
-        color, contents = line.split("bags contain ")
+        color, contents = line.split(" bags contain ")
         ruledict[color] = contents
 
     # now, via judicious use of regex, split the contents string into a list
@@ -21,7 +21,7 @@ def build_rules(file):
         # actually let's prep for part 2, because i expect to need the numbers
         # for it. Let's make contents another dict actually
         if contents == ['no other']:
-            ruledict[color] = contents
+            ruledict[color] = {'no other': 0}  # sure why not
         else:
             ruledict[color] = {color2: num for (num, color2) in (
                 s.split(maxsplit=1) for s in contents)}
@@ -33,11 +33,27 @@ def main():
     with open("input/day7.txt") as f:
         rules = build_rules(f)
 
-    temp = {}
-    for rule in rules:
-        if "salmon" in rule:
-            temp[rule] = rules[rule]
-    pprint.pprint(temp)
+    # temp = {}
+    # for rule in rules:
+    #     if "salmon" in rule:
+    #         temp[rule] = rules[rule]
+    # pprint.pprint(temp)
+
+    mybag = 'shiny gold'
+    bagqueue = [mybag]
+    output1 = []
+    while len(bagqueue) > 0:
+        testbag = bagqueue.pop()
+        for color in rules:
+            if testbag in rules[color]:
+                output1.append(color)
+                bagqueue.append(color)
+                # print(f"{color}!")
+
+    # remove duplicates by just doing set() at the end, screw it
+    print("Part 1:", len(set(output1)))
+
+    return output1
 
 
 if __name__ == '__main__':
