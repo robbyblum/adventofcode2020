fdec19a442763629a998ed66762f9e9124c2e0ce
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-08 01:51:06 -0500

    day 7 part 2 done!!!!

--- day7.py
+++ day7.py
@@ -23,10 +23,23 @@ def build_rules(file):
         if contents == ['no other']:
             ruledict[color] = {'no other': 0}  # sure why not
         else:
-            ruledict[color] = {color2: num for (num, color2) in (
+            ruledict[color] = {color2: int(num) for (num, color2) in (
                 s.split(maxsplit=1) for s in contents)}
 
     return ruledict
+
+
+# this does the recursive bag-counting for part 2
+def countbags(color, bagdict):
+    if color in bagdict:
+        subdict = bagdict[color]
+        m = 0
+        for subcolor in subdict:
+            n = subdict[subcolor]
+            m += n + n * countbags(subcolor, bagdict)
+        return m
+    else:
+        return 0
 
 
 def main():
@@ -39,6 +52,7 @@ def main():
     #         temp[rule] = rules[rule]
     # pprint.pprint(temp)
 
+    # part 1:
     mybag = 'shiny gold'
     bagqueue = [mybag]
     output1 = []
@@ -53,7 +67,27 @@ def main():
     # remove duplicates by just doing set() at the end, screw it
     print("Part 1:", len(set(output1)))
 
-    return output1
+    # return output1
+
+    # part 2
+    # actually don't need any of this part
+    # # First let's walk *down* from the shiny gold bag, to get the
+    # # list of colors we need to add up
+    # bagqueue = [mybag]
+    # output2 = []
+    # while len(bagqueue) > 0:
+    #     # i guess this is depth first, instead of breadth first?
+    #     testbag = bagqueue.pop()
+    #     for color in rules[testbag]:
+    #         if color != 'no other':
+    #             output2.append(color)
+    #             bagqueue.append(color)
+    #             print(f"in {testbag}: {color}!")
+    #         else:
+    #             print(f"in {testbag}: nothing!")
+    # print(len(output2))
+    # all I need is this!
+    print("Part 2:", countbags('shiny gold', rules))
 
 
 if __name__ == '__main__':
