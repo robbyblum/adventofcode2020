37e3ee5921d6ed66a69b81d2aa909ee77f185091
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-07 20:24:00 -0500

    day 6 part 2 done

--- day6.py
+++ day6.py
@@ -7,20 +7,26 @@ def parse_forms(file):
     raw = file.read()
     # Ths smushes each group's responses together, to better facilitate part 1.
     # I'm sure I'll regret this in part 2.
-    grouplist = ["".join(p.splitlines()) for p in raw.split("\n\n")]
-    # pdict = [dict(field.split(":") for field in p) for p in plist]
+    # grouplist = ["".join(p.splitlines()) for p in raw.split("\n\n")]
+
+    # yeah that doesn't work for part 2 but I can make it work. here
+    grouplist = [p.splitlines() for p in raw.split("\n\n")]
     return grouplist
 
 
 def main():
     with open("input/day6.txt") as f:
-        grouplist = parse_forms(f)
+        group_list = parse_forms(f)
 
     # part 1
-    sumcounts = 0
-    for group in grouplist:
-        sumcounts += len({c for c in group})
-    print("Part 1:", sumcounts)
+    sumcounts1 = 0
+    sumcounts2 = 0
+    for group in group_list:
+        groupset = {c for c in ''.join(group)}
+        sumcounts1 += len(groupset)
+        sumcounts2 += sum([all([d in e for e in group]) for d in groupset])
+    print("Part 1:", sumcounts1)
+    print("Part 2:", sumcounts2)
 
 
 if __name__ == '__main__':
