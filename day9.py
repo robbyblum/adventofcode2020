a401d9e8c02e18c424ab7f5d2f44d0d2d622ec02
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-09 00:37:52 -0500

    day 9 part 2 done

--- day9.py
+++ day9.py
@@ -20,9 +20,35 @@ def main():
             # Inner loop was broken, break the outer.
             break
         else:
-            print(f"Part 1: {currentN}")
+            print(f"Part 1: {currentN} at index {N}")
             break
 
+    part1answer = currentN
+    part1index = N
+    # part 2: let's suppose that the contiguous region is always *before* the
+    # special number. This is reasonable because while the sequence doesn't
+    # increase monotonically, it does increase overall. If this doesn't work
+    # I'll reevaluate.
+
+    for n in range(part1index - 1):
+        foundit = False
+        listsum = [input[n]]
+        for m in range(n + 1, part1index):
+            listsum.append(input[m])
+            if sum(listsum) < part1answer:
+                continue
+            elif sum(listsum) > part1answer:
+                break
+            elif sum(listsum) == part1answer:
+                foundit = True
+                print("you got it")
+                break
+        if foundit:
+            break
+    # print(n, m)
+    # print(listsum)
+    # print(min(listsum), max(listsum))
+    print(f"Part 2: {min(listsum) + max(listsum)}")
 
 if __name__ == '__main__':
     main()
