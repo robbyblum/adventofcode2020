7e119eaef7503bcdf423a3c9d31c7346594416a5
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-12 22:40:16 -0500

    day 12 part 2 done
    boaty mcboatface sails again

--- day12.py
+++ day12.py
@@ -14,13 +14,16 @@ import numpy as np
 class boat2d:
     """docstring for boaty mcboatface."""
 
-    def __init__(self, startpos=[0.0, 0.0], startdir=[1.0, 0.0]):
+    def __init__(self, startpos=[0.0, 0.0], startdir=[1.0, 0.0],
+                 startway=[10.0, 1.0]):
         """create ye boat"""
         # super(, self).__init__()  # leftover from template, meh
         # initial position, default the origin (0, 0)
         self.pos = np.array(startpos)
         # initial orientation, default east (+1, 0)
         self.dir = np.array(startdir)
+        # initial waypoint location (for part 2), default [10, 1]
+        self.way = np.array(startway)
 
     def rotate_arb(self, angle, degrees=True):
         """rotate ye boat...sadly for this problem I don't need to handle
@@ -66,6 +69,27 @@ class boat2d:
             head = "????"
         return f"position: {self.pos}    heading: {head} ({self.dir})"
 
+    # down here we define weird waypoint-related methods for part 2 of the
+    # problem. At least now we're not sliding the boat around...
+    def way_slide(self, vector):
+        """slide ye waypoint"""
+        self.way += vector
+
+    def way_rotate(self, angle):
+        """rotate ye waypoint about ye boat, 90 degree increments only"""
+        if angle % 90 != 0:
+            raise ValueError("Hey! Don't do that! 90 degree increments only!")
+        angle *= np.pi / 180
+        # This is ridiculous, and it produces nonsense if you give it angles
+        # that aren't in 90 degree increments. Hence the ValueError above.
+        rotmat = np.array([[np.cos(angle).round(), -np.sin(angle).round()],
+                          [np.sin(angle).round(), np.cos(angle).round()]])
+        self.way = rotmat @ self.way
+
+    def way_forward(self, N):
+        """move ye boat forward to ye waypoint N times"""
+        self.pos += N * self.way
+
 
 def load_instructions(file):
     """loads instructions from file, stores in cryptic tuples"""
@@ -74,10 +98,9 @@ def load_instructions(file):
     return dirs
 
 
-def translate_instruction(boat, instr):
+def translate_instruction_part1(boat, instr):
     """translates an instruction out of a cryptic tuple and applies it to the
     boat object instance specified"""
-
     # just a big ol' switch structure
     if instr[0] == "F":
         # move forward
@@ -102,6 +125,33 @@ def translate_instruction(boat, instr):
         boat.rotate(instr[1])
 
 
+def translate_instruction_part2(boat, instr):
+    """translates an instruction out of a cryptic tuple and applies it to the
+    boat object instance specified"""
+    # just a big ol' switch structure
+    if instr[0] == "F":
+        # move forward
+        boat.way_forward(instr[1])
+    elif instr[0] == "N":
+        # slide north
+        boat.way_slide([0, instr[1]])
+    elif instr[0] == "E":
+        # slide east
+        boat.way_slide([instr[1], 0])
+    elif instr[0] == "S":
+        # slide south
+        boat.way_slide([0, -instr[1]])
+    elif instr[0] == "W":
+        # slide west
+        boat.way_slide([-instr[1], 0])
+    elif instr[0] == "R":
+        # turn to starboard
+        boat.way_rotate(-instr[1])
+    elif instr[0] == "L":
+        # turn to port
+        boat.way_rotate(instr[1])
+
+
 def main():
     # x = boat2d()
     # print(x.info())
@@ -121,10 +171,20 @@ def main():
         directions = load_instructions(f)
 
     for instr in directions:
-        translate_instruction(boaty, instr)
+        translate_instruction_part1(boaty, instr)
 
     print(boaty.info())
     print("Part 1:", abs(boaty.pos).sum())
+
+    # reset ye boaty
+    del boaty
+    boaty = boat2d()
+
+    for instr in directions:
+        translate_instruction_part2(boaty, instr)
+
+    print(boaty.info())
+    print("Part 2:", abs(boaty.pos).sum())
 
 
 if __name__ == '__main__':
