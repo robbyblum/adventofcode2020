fbd0fbb55f6d2bef8cae96a32741e869f02075c4
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-14 17:54:53 -0500

    day 13 part 2 is not fun and i'm >:V about it

--- day13.py
+++ day13.py
@@ -12,15 +12,31 @@ def main():
                 continue
             else:
                 bus_list.append(int(bus))
-        print(bus_list)
+        print("bus list, condensed:", bus_list)
 
         wait_times = []
         for bus in bus_list:
             wait_times.append(bus - (t0 % bus))
-        print(wait_times)
+        print("wait times:", wait_times)
         waitmin_i = wait_times.index(min(wait_times))
-        print(f"Part 1: bus {bus_list[waitmin_i]}, {wait_times[waitmin_i]} minute wait: {bus_list[waitmin_i] * wait_times[waitmin_i]}")
+        minbus = bus_list[waitmin_i]
+        minwait = wait_times[waitmin_i]
+        print(f"Part 1: bus {minbus}, {minwait} minute wait: {minbus * minwait}")
 
+        # part 2, a rather different problem...
+        print("Part 2:")
+        L = len(bus_list_raw)
+        t_offsets = [i for i in range(L)]
+        t_off_list = []
+        for i, bus in enumerate(bus_list_raw):
+            if bus == 'x':
+                continue
+            else:
+                t_off_list.append(t_offsets[i])
+        print(bus_list)
+        print(t_off_list)
+        # the answer is below 1909273434898297, which is the product of all the
+        # bus numbers in question. So uh, that helps??? (does it really?)
 
 
 if __name__ == '__main__':
