52fe285324445589ca29b5bc462441ecd7fcd666
RLB <20372070+robbyblum@users.noreply.github.com>
2020-12-20 02:53:15 -0500

    day 18: stub

---
+++ day18.py
@@ -0,0 +1,4 @@
+# day 18: ugh, an algebra
+# evaluate the expressions but with different than normal operator precedence
+# addition and multiplication at the same level, evaluated left to right
+# parentheses take precedence over + or *, as usual
